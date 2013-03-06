443409834a171adbdd026c66e7a0b8daa42246ae
Prakash Gamit <prakashgamit23@gmail.com>
2013-03-06 13:48:59 +0530

    include standard header files

---
+++ netcat_core.h
@@ -0,0 +1,34 @@
+/* *********************************************************************** *
+ *       Filename:  netcat_core.h
+ *
+ *    Description:  define options struct & declare global variables
+ *
+ *        Version:  1.0
+ *        Created:  Wednesday 06 March 2013 01:31:03  IST
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Prakash Gamit (09211014), <prakashgamit23@gmail.com>
+ *   Organization:  Indian Institute of Technology, Roorkee
+ *
+ * ********************************************************************** */
+
+#ifndef _NETCAT_CORE_H_
+#define _NETCAT_CORE_H_
+
+
+/* include standar header files */
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <string.h>
+#include <strings.h>
+
+#include <unistd.h>
+#include <errno.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+
+
+#endif
