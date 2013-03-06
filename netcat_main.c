c94427c3c6f3bb2e5c5dc37269a9d79c015b393f
Prakash Gamit <prakashgamit23@gmail.com>
2013-03-06 14:29:47 +0530

    parse command line args

--- netcat_main.c
+++ netcat_main.c
@@ -21,5 +21,41 @@ int main(int argc, char *argv[]){
     /* initialize options */
     init_options();
 
+    /* parse command line args */
+    int c;
+    while((c = getopt(argc, argv, ":hve:p:lku")) != -1){
+        switch(c){
+            case 'h': help();
+                      exit(EXIT_SUCCESS);
+
+            case 'v': o.verbose = 1;
+                      break;
+
+            case 'e': o.execcommand = 1;
+                      o.command = optarg;
+                      break;
+
+            case 'p': o.port = atoi(optarg);
+                      break;
+
+            case 'l': o.listen = 1;
+                      break;
+
+            case 'k': o.keepopen = 1;
+                      break;
+
+            case 'u': o.udp = 1;
+                      break;
+
+            case ':': fprintf(stderr, "-%c requires argument\n", optopt);
+                      exit(EXIT_FAILURE);
+
+            /* case ? */
+            default: fprintf(stderr, "unknown option -%c\n", optopt);
+                     exit(EXIT_FAILURE);
+
+        }/* switch */
+    }/* while */
+
     return 0;
 }/* main() */
